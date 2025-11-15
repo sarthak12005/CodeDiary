f60e55f02d90c75c969ee8b4af86cda3c4d3f42e
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-11-15 17:18:21 +0530

    added navbar and header in the home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
     <>
-      Hello React
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/blog' element={<Blog/>} />
+      </Routes>
     </>
   )
 }
